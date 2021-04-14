b833e2c95c0af7e0078b0a9ef2eefda14a47b419
Nathan Pitman <pitmannathan@gmail.com>
2021-04-14 22:17:48 +1200

    Add Makefile, Bochs config, and random cross-compiler scratchpad notes

---
+++ build/setup-env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Install binutils (for ld)
+# Install gcc
+# Install nasm
+# Install make
+
+# Install bison
+# Install flex
+
+# Install MPFR
+https://askubuntu.com/questions/394296/how-to-install-mpfr-3-1-2-in-ubuntu-13-04
+
+# Install MPC
+
+# Install GMP
+sudo apt-get install libgmp3-dev
+
+sudo apt-get install g++
+# Install TexInfo
+
+
+
+
+# Install ISL
+# Instal CLooG
+
+
+
+
